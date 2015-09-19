06c2962e1f673012849c3e9723d298b2d0482da6
Ivan Grinkevich <grin.van@gmail.com>
2015-09-19 14:59:05 -0700

    changed the pins to what we need

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.LEFT_GARAGE_PIN = 16;
-config.RIGHT_GARAGE_PIN = 11;
+config.LEFT_GARAGE_PIN = 23;
+config.RIGHT_GARAGE_PIN = 24;
 
 config.RELAY_ON = 0;
 config.RELAY_OFF = 1;
